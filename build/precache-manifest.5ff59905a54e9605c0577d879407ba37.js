52c196e5bd2b0493ed7fccc3da85465b1be11902
Bannon Tanner <bannon.nicholas.tanner@ibm.com>
2019-11-22 13:49:43 -0600

    uibuild

---
+++ build/precache-manifest.5ff59905a54e9605c0577d879407ba37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2386d066009ac40b1ed08dda7f095d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f90db2469404f32d8f71",
+    "url": "/static/css/main.9fcc276b.chunk.css"
+  },
+  {
+    "revision": "8b4fe4f465547963097b",
+    "url": "/static/js/2.3d3dd40b.chunk.js"
+  },
+  {
+    "revision": "f90db2469404f32d8f71",
+    "url": "/static/js/main.9154f556.chunk.js"
+  },
+  {
+    "revision": "d1973d96e61dcdb1b4d5",
+    "url": "/static/js/runtime-main.039e5682.js"
+  }
+]);
